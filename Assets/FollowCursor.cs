5c1b1a0e28b9cb3ebbfb3e39036f90474abe9c59
Wolfleader <james.morgan@iinet.net.au>
2021-10-15 10:00:48 +0800

    Initial commit

---
+++ Assets/FollowCursor.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class FollowCursor : MonoBehaviour
+{
+    [SerializeField] private Camera mainCamera;
+    [SerializeField] private LayerMask layerMask;
+
+    private Vector2 _mousePos;
+
+    private void Start()
+    {
+        if (mainCamera == null)
+        {
+            mainCamera = Camera.main;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray ray = mainCamera.ScreenPointToRay(_mousePos);
+        if (Physics.Raycast(ray, out RaycastHit hit, float.MaxValue, layerMask))
+        {
+            this.transform.position = hit.point;
+        }
+    }
+
+    public void OnMouseMove(InputAction.CallbackContext context)
+    {
+        _mousePos = context.ReadValue<Vector2>();
+    }
+}
